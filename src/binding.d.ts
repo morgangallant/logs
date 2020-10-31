93f56b9ab85c6bc2742e4f18bb48d3e37b7951fc
Morgan Gallant <morgan@morgangallant.com>
2020-10-30 21:08:54 -0400

    integrated workers kv store
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

---
+++ src/binding.d.ts
@@ -0,0 +1,12 @@
+export {}
+
+declare global {
+  /**
+   * Cloudflare defined environment variables.
+   */
+
+  /**
+   * Workers KV namespace references.
+   */
+  const LDB: KVNamespace
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import { handleRequest } from './handler'
+import { Router } from './router'
 
+/**
+ * HandleRequest routes an incoming request to the appropriate
+ * handler, in addition to writing logs and handling errors.
+ * @param request
+ */
+const handleRequest = async (req: Request): Promise<Response> => {
+  const router = new Router()
+  try {
+    return await router.route(req)
+  } catch (except) {
+    // TODO: Add Sentry Logging.
+    return new Response(except.message || 'An error occured!', {
+      status: 500,
+    })
+  }
+}
+
+/**
+ * The entry point of the script for incoming web requests.
+ */
 addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request))
 })
